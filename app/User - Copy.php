091c24ec64424571c140fedf8a8b3b4b2fbb4903
minhazv14 <47391006+minhazv14@users.noreply.github.com>
2020-08-24 00:16:59 +0600

    changes

--- app/User - Copy.php
+++ app/User - Copy.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
+    *****///protected $fillable = [
         'name', 'email', 'password',
     ];
 
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+}
+*/
+protected $casts = [
+    'email_verified_at' => 'datetime',
+];
 }
